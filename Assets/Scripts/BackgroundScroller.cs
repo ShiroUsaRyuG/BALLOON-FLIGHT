91ff7d1f20eee02c66ed48c6cdae9a4a8d48b6f3
ShiroUsaRyuG <klonoa.eevee.tails@gmail.com>
2021-07-20 15:46:18 +0900

    7/20 15:45 進行状況1

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -6,8 +6,12 @@ public class BackgroundScroller : MonoBehaviour
 {
     [Header("背景画像のスクロール速度 = 強制スクロールの速度")]
     public float scrollSpeed = 0.01f;
-    public float stopPosition = -16f;
-    public float restartPosition = 5.8f;
+
+    [Header("画像のスクロール終了地点")]
+    public float stopPosition = -18f;
+
+    [Header("画像の再スタート地点")]
+    public float restartPosition = 7.6f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +23,7 @@ public class BackgroundScroller : MonoBehaviour
     void Update()
     {
         transform.Translate(-scrollSpeed, 0, 0);
+
         if (transform.position.x < stopPosition)
         {
             transform.position = new Vector2(restartPosition, 0);
